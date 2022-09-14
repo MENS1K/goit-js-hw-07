64c72bf32ed25d6839351e1af97ed489402ec720
Misha Panivnyk <panivnykm@gmail.com>
2022-09-14 13:38:34 +0300

    add task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const galleryRef = document.querySelector(".gallery");
+const listImg = galleryItems.map((item) => {
+  return `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image"  src="${item.preview}"    alt="${item.description}" />
+</a>`;
+});
+galleryRef.insertAdjacentHTML("afterbegin", listImg.join(" "));
+new SimpleLightbox(".gallery a", {
+  captionDelay: 300,
+  captionsData: "alt",
+});
 console.log(galleryItems);
